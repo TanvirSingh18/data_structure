28d775e89bf6b39a1cc5990663fecb74b90a7b5d
TanvirSingh18 <122678623+TanvirSingh18@users.noreply.github.com>
2023-04-21 20:29:21 +0530

    Create 2darray.c

---
+++ 2darray.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+// Main function
+int main(){
+    // Declare variables
+    int i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
+    // Declare array
+    int a[5][5];
+    // Input array elements
+    for(i=0; i<5; i++){
+        for(j=0; j<5; j++){
+            printf("Enter a[%d][%d]: ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    // Print array elements
+    for(k=0; k<5; k++){
+        for(l=0; l<5; l++){
+            printf("%d\t", a[k][l]);
+        }
+        printf("\n");
+}
+    return 0;
+}
